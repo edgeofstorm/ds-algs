81ccf36cd49f59c4cdcb4cb03b467c1fb328a6ed
edgeofstorm <enesekinci1907@gmail.com>
2024-09-07 17:15:36 +0300

    is palindrome int

---
+++ questions/is_palindrome_int.py
@@ -0,0 +1,46 @@
+"""
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+"""
+
+
+def solution(x):
+    if x < 0:
+        return False
+
+    reversed_num = 0
+    temp = x
+
+    while temp != 0:
+        digit = temp % 10
+        reversed_num = reversed_num * 10 + digit
+        temp //= 10
+
+    return reversed_num == x
+
+
+if __name__ == "__main__":
+    assert solution(123) == False
+    assert solution(1221) == True
+    assert solution(456654) == True
+    assert solution(10) == False
+    assert solution(100) == False
